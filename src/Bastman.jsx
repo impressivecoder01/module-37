de3f9d8abf1758f7cf8d732443161177d6ac6e9e
impressivecoder1 <rohanrahman251@gmail.com>
2025-12-25 01:55:59 +0600

    class four

--- src/Bastman.jsx
+++ src/Bastman.jsx
@@ -1,17 +1,28 @@
 import { useState } from "react"
 
 export default function Batsman(){
-    let [score, setScore]= useState(0)
+    const [score, setScore]= useState(0)
+    const [six, countSix] = useState(0)
+    const [four, countFour] = useState(0)
+    // const countSixes = ()=>{
+    //     let sixes = six + 0
+    //     countSix(sixes)
+
+    // }
     const handleSingle = ()=>{
         const update = score + 1
         setScore(update)
     }
     const handleFour = ()=>{
         let updated = score +4
+        let updatedFour = four + 1;
+        countFour(updatedFour)
         setScore(updated)
     }
     const handleSix = ()=>{
         let update = score + 6;
+        let updateSixes = six + 1
+        countSix(updateSixes)
         setScore(update)
     }
     return(
@@ -24,6 +35,8 @@ export default function Batsman(){
             {
                 <p>Now your score is {score}</p>
             }
+            <p>Six: {six}</p>
+            <p>Four: {four}</p>
             <h1>Score:{score}</h1>
             <button onClick={()=>handleSingle()}>Singles</button>
             <button onClick={()=>handleFour()}>Four</button>
